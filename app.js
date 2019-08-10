7030321e4466773080435c41780a0f722a9afac3
Luke Swan <lukeswan23@gmail.com>
2019-08-10 15:35:39 +1000

    added score functionality fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const chooseMoveEls = document.querySelector(".rps__chooseMove");
 let resultText = document.querySelector(".rps__game__result").childNodes[1];
 const playerImg = gameEls.childNodes[1];
 const cpuImg = gameEls.childNodes[3];
+const scoreEl = document.querySelector(".score").childNodes[0];
 
 //listener for the img, passes on the alt text which is used to identify the move chosen
 const chooseMove = () => {
@@ -18,6 +19,18 @@ const chooseMove = () => {
   });
 };
 
+let scoreKeep = 0;
+
+const score = condition => {
+  //console.log(score);
+  if (condition == "win") {
+    scoreKeep++;
+  } else {
+    scoreKeep = 0;
+  }
+  scoreEl.textContent = `Your streak is: ${scoreKeep}`;
+};
+
 const gameLogic = (moveOne, moveTwo) => {
   if (moveOne == moveTwo) {
     return (resultText.textContent = "You Tie!");
@@ -25,20 +38,26 @@ const gameLogic = (moveOne, moveTwo) => {
 
   if (moveOne == "paper") {
     if (moveTwo == "scissors") {
+      score();
       return (resultText.textContent = `You lose to ${moveTwo}!`);
     }
+    score("win");
     return (resultText.textContent = `You win with ${moveOne}!`);
   }
   if (moveOne == "scissors") {
     if (moveTwo == "rock") {
+      score();
       return (resultText.textContent = `You lose to ${moveTwo}!`);
     }
+    score("win");
     return (resultText.textContent = `You win with ${moveOne}!`);
   }
   if (moveOne == "rock") {
     if (moveTwo == "paper") {
+      score();
       return (resultText.textContent = `You lose to ${moveTwo}!`);
     }
+    score("win");
     return (resultText.textContent = `You win with ${moveOne}!`);
   }
 };
@@ -58,7 +77,7 @@ const game = (player, cpu) => {
 
 const cpuMove = () => {
   let choice = 1 + Math.floor(Math.random() * 3);
-  console.log(choice);
+  //console.log(choice);
   switch (choice) {
     case 1:
       return "paper";
@@ -88,7 +107,7 @@ let animateHands = () => {
   setTimeout(function() {
     cpuImg.classList.remove("handAnimate");
     playerImg.classList.remove("handAnimate");
-  }, 400);
+  }, 1000);
 };
 
 chooseMove();
